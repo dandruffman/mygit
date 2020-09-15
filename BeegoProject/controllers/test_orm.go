dc593191e1eec788701d8ba36fd75d7b365ae128
wangwu <wangwu@qq.com>
2020-09-15 23:31:16 +0800

    Basic functions completed

--- BeegoProject/controllers/test_orm.go
+++ BeegoProject/controllers/test_orm.go
@@ -15,6 +15,14 @@ type TestOrmAddController struct {
 	beego.Controller
 }
 
+type TestOrmDeleteController struct {
+	beego.Controller
+}
+
+type TestOrmUpdateController struct {
+	beego.Controller
+}
+
 func (t *TestOrmListController) Get()  {
 	o :=orm.NewOrm()
 	var users []models.Users
@@ -43,5 +51,45 @@ func (t *TestOrmAddController) Post()  {
 	o :=orm.NewOrm()
 	users :=models.Users{Username:username}
 	o.Insert(&users)
-	t.TplName = "list.html"
+	t.Redirect("/test_orm_list",302)
 }
+
+func (t *TestOrmDeleteController) Get()  {
+	id, _ :=t.GetInt("id")
+	fmt.Print(id)
+
+	o :=orm.NewOrm()
+	users :=models.Users{Id:id}
+	o.Delete(&users)
+	t.Redirect("/test_orm_list",302)
+}
+
+func (t *TestOrmUpdateController) Get()  {
+	id, _ :=t.GetInt("id")
+	fmt.Print(id)
+
+	o :=orm.NewOrm()
+	users :=models.Users{Id:id}
+	o.Read(&users)
+
+	t.Data["users"] = users
+	t.TplName = "edit.html"
+}
+
+func (t *TestOrmUpdateController) Post()  {
+	username :=t.GetString("username")
+	fmt.Print(username)
+	id, _ :=t.GetInt("id")
+	fmt.Print(id)
+
+	o :=orm.NewOrm()
+	//users :=models.Users{Id:1}
+
+
+	users :=models.Users{Id:id}
+	o.Read(&users)
+	users.Username=username
+	o.Update(&users)
+	t.Redirect("/test_orm_list",302)
+}
+
--- BeegoProject/routers/router.go
+++ BeegoProject/routers/router.go
@@ -10,4 +10,6 @@ func init() {
 	//增删改查
 	beego.Router("/test_orm_list", &controllers.TestOrmListController{})
 	beego.Router("/test_orm_add", &controllers.TestOrmAddController{})
+	beego.Router("/test_orm_delete", &controllers.TestOrmDeleteController{})
+	beego.Router("/test_orm_update", &controllers.TestOrmUpdateController{})
 }
